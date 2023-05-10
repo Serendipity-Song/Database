d203d038e519369fbb6d9e4aedf2852755bdfe3a
Serendipity-Song <thdalwh3867@gmail.com>
2023-05-10 21:01:18 +0900

    update 4-9

--- 4. Intermediate SQL/9.sql
+++ 4. Intermediate SQL/9.sql
@@ -1,7 +1,7 @@
 CREATE TABLE manager (
- employee_id char(20),
+ employee_id char(20), ---숫자 지정, id값 character
  manager_id char(20),
  PRIMARY KEY employee_id,
- FOREIGN KEY (manager_id) REFERENCES manager (employee_id)
+ FOREIGN KEY (manager_id) REFERENCES manager (employee_id) --- Reference 참조를 통해 기존 테이블을 다시 recursive하게 이용.
  ON DELETE CASCADE
 );
